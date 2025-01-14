33a0bf6dcfd4fdba96192192498bea5d928b9368
ton-ayr <ayrtoncbarreira@gmail.com>
2025-01-14 20:40:32 -0300

    Criado funções para desconsiderar caracteres especiais e curtos, objetos vazios e separados por paragrafo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 const fs = require('fs');
 
-const arquivo = process.argv;
-const link = arquivo[2];
+const caminhoArquivo = process.argv;
+const link = caminhoArquivo[2]; 
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    console.log(texto);
-}) 
+  quebraEmParagrafos(texto);
+})
+
+function quebraEmParagrafos(texto) {
+  const paragrafos = texto.toLowerCase().split('\n');
+  const contagem = paragrafos.map((paragrafo) => {
+      if (!paragrafo) return null;
+      const resultado = verificaPalavrasDuplicadas(paragrafo);
+      return Object.keys(resultado).length > 0 ? resultado : null;
+    }).filter((item) => item !== null);
+
+  console.log(contagem);
+}
+
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\r]/g, '');
+}
+
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(' ');
+  const resultado = {};
+  listaPalavras.forEach((palavra) => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+    }
+  });
+  return resultado;
+}
