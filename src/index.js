69d168311ffab508cbb4dce2ef05fa102b4ca106
ton-ayr <ayrtoncbarreira@gmail.com>
2025-01-16 20:18:08 -0300

    Separando funções e mapeando erros

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
-const fs = require('fs');
-
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2]; 
-
-fs.readFile(link, 'utf-8', (erro, texto) => {
-  quebraEmParagrafos(texto);
-})
-
-function quebraEmParagrafos(texto) {
-  const paragrafos = texto.toLowerCase().split('\n');
-  const contagem = paragrafos.map((paragrafo) => {
-      if (!paragrafo) return null;
-      const resultado = verificaPalavrasDuplicadas(paragrafo);
-      return Object.keys(resultado).length > 0 ? resultado : null;
-    }).filter((item) => item !== null);
-
+export function contaPalavras(texto) {
+  const paragrafos = extraiParagrafos(texto)
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+  })
   console.log(contagem);
+}
+
+function extraiParagrafos(texto) {
+  return texto.toLowerCase().split('\n');
 }
 
 function limpaPalavras(palavra) {
@@ -25,11 +18,12 @@ function limpaPalavras(palavra) {
 function verificaPalavrasDuplicadas(texto) {
   const listaPalavras = texto.split(' ');
   const resultado = {};
-  listaPalavras.forEach((palavra) => {
+  listaPalavras.forEach(palavra => {
     if (palavra.length >= 3) {
       const palavraLimpa = limpaPalavras(palavra);
-      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
     }
-  });
+  })
   return resultado;
 }
+
